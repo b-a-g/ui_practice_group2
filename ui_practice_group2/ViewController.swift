baccd86ba9bc3f479baead05e15d56b4ebdf849b
aleksandr-belyaev <belyaev.photo@gmail.com>
2021-05-08 12:27:11 +0700

    Initial Commit

---
+++ ui_practice_group2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ui_practice_group2
+//
+//  Created by Александр Беляев on 08.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
